0c023e6e5398289725ff8ef2ce102a6de95b1807
ervanherdiansyah <77282241+ervanherdiansyah@users.noreply.github.com>
2024-11-26 14:22:57 +0700

    first commit

---
+++ app/Models/ProjectMemberships.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ProjectMemberships extends Model
+{
+    use HasFactory;
+    protected $guarded = [
+        'id'
+    ];
+    public function project()
+    {
+        return $this->belongsTo(Projects::class, 'project_id');
+    }
+
+    public function invitedUser()
+    {
+        return $this->belongsTo(User::class, 'invited_user_id');
+    }
+}
---
+++ app/Models/Projects.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Projects extends Model
+{
+    use HasFactory;
+    protected $guarded = [
+        'id'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function invitations()
+    {
+        return $this->hasMany(ProjectMemberships::class, 'project_id');
+    }
+}
