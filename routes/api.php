a48890f5a96b6f923ea75c2f27e14a93c4aff8e1
ervanherdiansyah <77282241+ervanherdiansyah@users.noreply.github.com>
2024-12-01 06:18:17 +0700

    update

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Authentication\AuthController;
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -28,3 +29,5 @@ Route::group([
     Route::post('refresh', [AuthController::class, 'refresh']);
     Route::post('me', [AuthController::class, 'me']);
 });
+
+Route::get('/test', [Controller::class, 'test']);
