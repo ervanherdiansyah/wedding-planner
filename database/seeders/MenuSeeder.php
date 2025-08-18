e07fe995e7ba770b293a5b58cc41dfb360e32f4c
Ervan Herdiansyah <ervan@tech.kelola.biz>
2025-08-18 10:45:31 +0700

    update

--- database/seeders/MenuSeeder.php
+++ database/seeders/MenuSeeder.php
@@ -16,6 +16,8 @@ class MenuSeeder extends Seeder
         'icon' => null,
         'is_active' => 1,
         'parent' => null,
+        'order' => 0,
+        'url' => '/dashboards',
         'permissions' => []
       ],
       [
@@ -24,6 +26,8 @@ class MenuSeeder extends Seeder
         'icon' => 'overviews',
         'is_active' => 1,
         'parent' => 1,
+        'order' => 1,
+        'url' => '/dashboards/overviews',
         'permissions' => ['Create', 'Read', 'Update', 'Delete']
       ],
       [
@@ -32,6 +36,8 @@ class MenuSeeder extends Seeder
         'icon' => null,
         'is_active' => 1,
         'parent' => null,
+        'order' => 2,
+        'url' => '/informasi-pernikahan',
         'permissions' => []
       ],
       [
@@ -40,6 +46,8 @@ class MenuSeeder extends Seeder
         'icon' => 'informasi-pengantin',
         'is_active' => 1,
         'parent' => 3,
+        'order' => 3,
+        'url' => '/informasi-pernikahan/informasi-pengantin',
         'permissions' => ['Create', 'Read', 'Update', 'Delete']
       ],
       [
@@ -48,6 +56,8 @@ class MenuSeeder extends Seeder
         'icon' => 'informasi-acara',
         'is_active' => 1,
         'parent' => 3,
+        'order' => 4,
+        'url' => '/informasi-pernikahan/informasi-acara',
         'permissions' => ['Create', 'Read', 'Update', 'Delete']
       ],
       [
@@ -56,6 +66,8 @@ class MenuSeeder extends Seeder
         'icon' => 'panitia',
         'is_active' => 1,
         'parent' => 3,
+        'order' => 5,
+        'url' => '/informasi-pernikahan/panitia',
         'permissions' => ['Create', 'Read', 'Update', 'Delete']
       ],
       [
@@ -64,6 +76,8 @@ class MenuSeeder extends Seeder
         'icon' => 'list-peserta-foto',
         'is_active' => 1,
         'parent' => 3,
+        'order' => 2,
+        'url' => '/informasi-pernikahan/list-peserta-foto',
         'permissions' => ['Create', 'Read', 'Update', 'Delete']
       ],
       [
@@ -72,6 +86,8 @@ class MenuSeeder extends Seeder
         'icon' => 'list-tamu-vip',
         'is_active' => 1,
         'parent' => 3,
+        'order' => 0,
+        'url' => '/informasi-pernikahan/list-tamu-vip',
         'permissions' => ['Create', 'Read', 'Update', 'Delete']
       ],
       [
@@ -80,6 +96,8 @@ class MenuSeeder extends Seeder
         'icon' => 'song-list',
         'is_active' => 1,
         'parent' => 3,
+        'order' => 1,
+        'url' => '/informasi-pernikahan/song-list',
         'permissions' => ['Create', 'Read', 'Update', 'Delete']
       ],
       [
@@ -88,6 +106,8 @@ class MenuSeeder extends Seeder
         'icon' => null,
         'is_active' => 1,
         'parent' => null,
+        'order' => 2,
+        'url' => '/perencanaan',
         'permissions' => []
       ],
       [
@@ -96,6 +116,8 @@ class MenuSeeder extends Seeder
         'icon' => 'budget',
         'is_active' => 1,
         'parent' => 10,
+        'order' => 3,
+        'url' => '/perencanaan/budget',
         'permissions' => ['Create', 'Read', 'Update', 'Delete']
       ],
       [
@@ -104,6 +126,8 @@ class MenuSeeder extends Seeder
         'icon' => 'to-do-list',
         'is_active' => 1,
         'parent' => 10,
+        'order' => 4,
+        'url' => '/perencanaan/to-do-list',
         'permissions' => ['Create', 'Read', 'Update', 'Delete']
       ],
       [
@@ -112,6 +136,8 @@ class MenuSeeder extends Seeder
         'icon' => 'vendor',
         'is_active' => 1,
         'parent' => 10,
+        'order' => 5,
+        'url' => '/perencanaan/vendor',
         'permissions' => ['Create', 'Read', 'Update', 'Delete']
       ],
       [
@@ -120,6 +146,8 @@ class MenuSeeder extends Seeder
         'icon' => 'seragam',
         'is_active' => 1,
         'parent' => 10,
+        'order' => 2,
+        'url' => '/perencanaan/seragam',
         'permissions' => ['Create', 'Read', 'Update', 'Delete']
       ],
       [
@@ -128,6 +156,8 @@ class MenuSeeder extends Seeder
         'icon' => 'rundown',
         'is_active' => 1,
         'parent' => 10,
+        'order' => 0,
+        'url' => '/perencanaan/rundown',
         'permissions' => ['Create', 'Read', 'Update', 'Delete']
       ],
       [
@@ -136,9 +166,95 @@ class MenuSeeder extends Seeder
         'icon' => 'seserahan',
         'is_active' => 1,
         'parent' => 10,
+        'order' => 1,
+        'url' => '/perencanaan/seserahan',
+        'permissions' => ['Create', 'Read', 'Update', 'Delete']
+      ],
+      // Tambahan Master Data
+      [
+        'name' => 'Master Data',
+        'slug' => 'master-data',
+        'icon' => null,
+        'is_active' => 1,
+        'parent' => null,
+        'order' => 3,
+        'url' => '/master-data',
+        'permissions' => []
+      ],
+      [
+        'name' => 'User',
+        'slug' => 'user',
+        'icon' => 'user',
+        'is_active' => 1,
+        'parent' => 17,
+        'order' => 0,
+        'url' => '/master-data/user',
+        'permissions' => ['Create', 'Read', 'Update', 'Delete']
+      ],
+      // Tambahan Transaction
+      [
+        'name' => 'Transaction',
+        'slug' => 'transaction',
+        'icon' => null,
+        'is_active' => 1,
+        'parent' => null,
+        'order' => 4,
+        'url' => '/transaction',
+        'permissions' => []
+      ],
+      [
+        'name' => 'Order',
+        'slug' => 'order',
+        'icon' => 'order',
+        'is_active' => 1,
+        'parent' => 19,
+        'order' => 0,
+        'url' => '/transaction/order',
+        'permissions' => ['Create', 'Read', 'Update', 'Delete']
+      ],
+      [
+        'name' => 'Payment',
+        'slug' => 'payment',
+        'icon' => 'payment',
+        'is_active' => 1,
+        'parent' => 19,
+        'order' => 1,
+        'url' => '/transaction/payment',
+        'permissions' => ['Create', 'Read', 'Update', 'Delete']
+      ],
+      // Tambahan Settings
+      [
+        'name' => 'Settings',
+        'slug' => 'settings',
+        'icon' => null,
+        'is_active' => 1,
+        'parent' => null,
+        'order' => 5,
+        'url' => '/settings',
+        'permissions' => []
+      ],
+      [
+        'name' => 'Menu',
+        'slug' => 'menu',
+        'icon' => 'menu',
+        'is_active' => 1,
+        'parent' => 22,
+        'order' => 0,
+        'url' => '/settings/menu',
+        'permissions' => ['Create', 'Read', 'Update', 'Delete']
+      ],
+      [
+        'name' => 'Package',
+        'slug' => 'package',
+        'icon' => 'package',
+        'is_active' => 1,
+        'parent' => 22,
+        'order' => 1,
+        'url' => '/settings/package',
         'permissions' => ['Create', 'Read', 'Update', 'Delete']
       ]
     ];
+
 
     $menuIds = [];
     $permissions = [];
@@ -162,7 +278,7 @@ class MenuSeeder extends Seeder
       // Tambahkan permission
       foreach ($menuData['permissions'] as $action) {
         $permissions[] = [
-          'name' => "{$action} {$menuData['title']}",
+          'name' => "{$action} {$menuData['name']}",
           'created_at' => now(),
           'updated_at' => now()
         ];
