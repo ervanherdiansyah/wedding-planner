bffb7a81c8a7a15518eb24b277f6260498ea34d2
Ervan Herdiansyah <ervan@tech.kelola.biz>
2025-09-30 16:55:43 +0700

    update

--- app/Models/MenuPackage.php
+++ app/Models/MenuPackage.php
@@ -2,12 +2,24 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class MenuPackage extends Model
 {
-    use HasFactory;
-
     protected $guarded = ['id'];
+
+    public function menu()
+    {
+        return $this->belongsTo(Menu::class);
+    }
+
+    public function permission()
+    {
+        return $this->belongsTo(Permission::class);
+    }
+
+    public function package()
+    {
+        return $this->belongsTo(Package::class);
+    }
 }
